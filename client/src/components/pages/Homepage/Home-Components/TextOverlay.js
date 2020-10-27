a8904ae0ece212ebbca99e6bed8ef06022296da3
viljaa <viljaacode@gmail.com>
2020-10-28 00:56:30 +0200

    Corrected typo

--- client/src/components/pages/Homepage/Home-Components/TextOverlay.js
+++ client/src/components/pages/Homepage/Home-Components/TextOverlay.js
@@ -8,7 +8,7 @@ const TextOverlay = (props) =>{
         <div className={props.class}>
             <div className='tile is-child is-8'>
                 <h2 className='title is-2 imgOverlay_text'>View your Steam achievements as a single collection.</h2>
-                <p className='imgOverlay_text'>Arch Steam Achievement Viewer fetches your Steam achievement data and compiles it as a viewable collection. See your achievements in one place, wihtout hassle.</p>
+                <p className='imgOverlay_text'>Arch Steam Achievement Viewer fetches your Steam achievement data and compiles it as a viewable collection. See your achievements in one place, without hassle.</p>
             </div>
         </div>
     )
